30c092580adbabfb87d5c5ed3a7f8cf82c4ec09c
rishavkali03 <153490565+rishavkali03@users.noreply.github.com>
2023-12-17 19:19:58 +0530

    script file and style file uploaded

---
+++ script.js
@@ -0,0 +1,45 @@
+let input = document.getElementById('input')
+let spans = document.querySelectorAll('span')
+let bar = document.querySelector('.bar')
+let random = 0
+
+
+
+
+
+spans.forEach(span => {
+    span.addEventListener('click', (e) => {
+        let number = e.target.textContent
+        if (number == "✖") {
+            input.value = input.value.slice(0, -1)
+        } else if (number == "✓") {
+            showResult()
+        } else {
+            input.value += number
+        }
+
+    })
+})
+
+
+function showResult() {
+    if (!input.value) return
+    let accuracy = 100 - Math.abs(input.value - random)
+    alert("Your Accuracy is " + accuracy + "%\n" + "You have enterd " + input.value + " but Acctual value is " + random)
+    FillAccuarium()
+}
+
+
+function FillAccuarium() {
+
+    input.value = null
+    random = Math.floor(Math.random() * 100)
+
+    bar.style.height = `${random}%`
+}
+
+
+
+
+
+FillAccuarium()
